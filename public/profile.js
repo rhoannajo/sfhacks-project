be5c8affd1d02636e2d69abef24b2aa7b7c19c47
Tolby Lam <tolbylam@gmail.com>
2021-03-07 11:06:39 -0800

    renamed files

--- public/profile.js
+++ public/profile.js
@@ -17,7 +17,7 @@ firebase.auth().onAuthStateChanged(function(newUser) {
 });
 
 document.getElementById('saveProfile').addEventListener('click', function(ev) {
-  const db = firebase.firestore();
+  const db = firebase.database();
   var docRef = db.collection('customers').doc(user.email);
   docRef.set({
     name: document.getElementById('customerName').value,
